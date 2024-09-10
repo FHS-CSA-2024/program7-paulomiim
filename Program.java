edd97de81a038aa8bd11067d98072bf1baf27dd5
paulomiim <paulomimehta0208@yahoo.com>
2024-09-09 23:51:46 -0700

    Update Program.java
    completed program 7.

--- Program.java
+++ Program.java
@@ -1,8 +1,55 @@
 //import stuff here
+import java.util.Scanner;
 
-//Your code here
+//declare variables
+public class Program7 {
+
+    public static void main (String args[]) {
+        
+        //call scanner
+        Scanner myScanner = new Scanner(System.in);
+        
+        //intialize variables
+        double schruteBucks = 0.0;
+        double klevins = 0.0;
+        double stanleyNickels = 0.0;
+        double decSchruteBuck = 0.0;
+        
+        //ask for user input
+        System.out.println("Enter schrute-bucks: "); 
+        schruteBucks = myScanner.nextDouble();
+        
+        System.out.println("Enter klevins: ");
+        klevins = myScanner.nextDouble();
+        
+        System.out.println("Enter stanley-nickels ");
+        stanleyNickels = myScanner.nextDouble();
+        
+        //perform calculations
+        klevins = klevins / 20.0;
+        stanleyNickels = stanleyNickels / 240;
+        decSchruteBuck = (schruteBucks + klevins + stanleyNickels) + 0.005;
+        
+        //casting
+        decSchruteBuck = (int)(decSchruteBuck * 100);
+        decSchruteBuck = (double)(decSchruteBuck / 100);
+        
+        //output
+        System.out.println ("Decimal schrute-bucks = $" + decSchruteBuck);
+        
+    }
+}
+
 
 //Paste console output below:
+/*Enter schrute-bucks: 
+7
+Enter klevins: 
+17
+Enter stanley-nickels 
+9
+Decimal schrute-bucks = $7.89
+
 /*
 
 
